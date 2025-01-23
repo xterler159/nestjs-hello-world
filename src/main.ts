e23a7b009e88680c1426a648fa9db1344d2e32a6
Kevin <kevin.fernando@groupe-creative.fr>
2025-01-23 12:13:18 +0100

    using Fastify instead of express.js

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
-import { NestFactory } from '@nestjs/core';
 import * as process from 'node:process';
+
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT as string;
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
   await app.listen(PORT);
 }
 bootstrap();
