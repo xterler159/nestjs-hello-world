6206a26e2a53b108932d1d79d48253fa96ae3020
Kevin <kevin.fernando@groupe-creative.fr>
2025-01-23 12:03:27 +0100

    installed @nestjs/config in order to handle env files + returning hello world in json format instead of an HTML page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+export type HelloWorld = {
+  hello: string;
+};
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): HelloWorld {
     return this.appService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'node:process';
 
 async function bootstrap() {
+  const PORT = process.env.PORT as string;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(PORT);
 }
 bootstrap();
