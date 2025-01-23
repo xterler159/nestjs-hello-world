52f9ea5666b17f3e39b6663673f3bdb9a2be8e85
Kevin <kevin.fernando@groupe-creative.fr>
2025-01-23 12:16:23 +0100

    added host utilization in the env file

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT as string;
+  const HOST = process.env.HOST as string;
 
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
 
-  await app.listen(PORT);
+  await app.listen(PORT, HOST);
 }
 bootstrap();
