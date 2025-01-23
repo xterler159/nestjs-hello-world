110e9639beb3fc52e140533869f97455dbbf2c3f
Kevin <kevin.fernando@groupe-creative.fr>
2025-01-23 12:41:09 +0100

    created cats ressource (CRUD)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,13 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { CatsModule } from './cats/cats.module';
+
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: ['.env', '.env.local'] })],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: ['.env', '.env.local'] }),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
